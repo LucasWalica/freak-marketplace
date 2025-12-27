aa32654f4b46a0bff9255dab1e7942ccc21636ea
LucasWalica <lucasw58@educastur.es>
2025-12-27 20:55:51 +0100

    working on categories backend

--- backend/categories/urls.py
+++ backend/categories/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import CategoryListView, CategoryDetailView
 
 urlpatterns = [
-    path("categories/", CategoryListView.as_view()),
-    path("categories/<int:id>/", CategoryDetailView.as_view()),
+    path("", CategoryListView.as_view()),
+    path("<int:id>/", CategoryDetailView.as_view()),
 ]
