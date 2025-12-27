44929cfae2437aa8acc7d596787f466cf09f021f
LucasWalica <lucasw58@educastur.es>
2025-12-27 20:48:25 +0100

    working on categories backend

--- backend/categories/views.py
+++ backend/categories/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from rest_framework.permissions import AllowAny
+from rest_framework import generics
+from .models import Category
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import status
+from .serializers import CategorySerializer
+from rest_framework.authtoken.models import Token
 # Create your views here.
+
+class CategoryListView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [AllowAny]
+
+class CategoryDetailView(generics.RetrieveAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [AllowAny]
+    lookup_field = 'id'
+
+
