fcfe1ab10aee58233f8cd41d8f5ef585fc9e7b68
LucasWalica <lucasw58@educastur.es>
2026-01-03 13:19:23 +0100

    improved product serializer

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -6,3 +6,14 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+    def validate(self, data):
+        category = data.get('category')
+        specs = data.get('specifications', {})
+        
+        # Validamos que todas las llaves del schema de la categoría estén en specs
+        for field in category.schema:
+            if field not in specs:
+                raise serializers.ValidationError(f"Falta el campo obligatorio: {field}")
+        
+        return data
